aaa213df336ecd767c8e1f2642c34bd9984342bb
Nguyen Van Thao <thaonguyen19962802@gmail.com>
2022-03-03 12:23:48 +0700

    update "home" page

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/api/auth/")
+    @ResponseBody
+    public String welcome(){
+        return "Welcome to main";
+    }
+
+    @RequestMapping("/api/auth/home")
+    public String home(){
+        return "index";
+    }
+}
